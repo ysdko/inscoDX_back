f4649637e4c36a803eea525af0c02119e174dc7f
ysdko <koki.yoshida110@gmail.com>
2021-08-28 04:00:32 +0900

    音声APIを返す

--- server.py
+++ server.py
@@ -1,9 +1,21 @@
+# -*- coding: utf-8 -*-
 import flask
+from flask import *
 import requests
 from dotenv import load_dotenv
 import os
+import ffmpeg
+
+
 
 app = flask.Flask(__name__)
+
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    return response
 
 @app.route('/', methods=['GET'])
 def index():
@@ -14,30 +26,50 @@ def index():
     </form>
 '''
 
+
+@app.route('/hello')
+def hello_world():
+    return jsonify({'message': 'Hello, world'})
+
 @app.route('/upload', methods=['POST'])
 def upload():
-    if 'file' not in flask.request.files:
-        return 'ファイル未指定'
 
-    fs = flask.request.files['file']
+    # print(request.files)
+        print("test")
+        if 'file' not in flask.request.files:
+            return 'no-file'
 
-    app.logger.info('file_name={}'.format(fs.filename))
-    app.logger.info('content_type={} content_length={}, mimetype={}, mimetype_params={}'.format(
-        fs.content_type, fs.content_length, fs.mimetype, fs.mimetype_params))
+        fs = flask.request.files['file']
 
-    fs.save('test.wav')
-    url ='https://api.webempath.net/v2/analyzeWav'
+        filename = fs.filename
+        fs.save(filename)
+        
+        # 入力
+        stream = ffmpeg.input(filename)
+        
+        # 出力
+        stream = ffmpeg.output(stream, "test.wav", ac=1, ar=11025).overwrite_output()
+        
+        try:
+        # 実行
+            ffmpeg.run(stream)
+        except Error as e:
+            print(e)
 
-    # .envにAPI_KEY = xxxx を記述する
-    apikey = os.getenv('API_KEY')
-    payload = {'apikey': apikey}
-    wav = "test.wav" 
-    data = open(wav, 'rb')
-    file = {'wav': data}
-    res = requests.post(url, params=payload, files=file)
-    print(res.json())
+        url ='https://api.webempath.net/v2/analyzeWav'
 
-    return "フィアルアップロード成功"
+        # .envにAPI_KEY = xxxx を記述する
+        apikey = os.getenv('API_KEY')
+        payload = {'apikey': apikey}
+        wav = "test.wav"
+        data = open(wav, 'rb')
+        file = {'wav': data}
+        res = requests.post(url, params=payload, files=file)
+
+        print(res.json())
+
+        return res.json()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+    # app.run(debug=True)
